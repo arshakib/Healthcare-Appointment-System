b254ee73a6fa37cbd2216d62e417779eaf03d407
shakib <atking2240@gmail.com>
2025-03-25 20:41:17 +0600

    Add existing project files to Git

--- app/page.jsx
+++ app/page.jsx
@@ -95,6 +95,13 @@ export default function Home() {
             width={16}
             height={16}
           />
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
           Go to nextjs.org →
         </a>
       </footer>
