21eb108a7a57b061bb7819499369e68f944efe14
jahidhasan6676 <jh18186676@gmail.com>
2025-03-27 16:21:55 +0600

    Resolve merge conflict

--- componenet/DashboardNav.jsx
+++ componenet/DashboardNav.jsx
@@ -2,8 +2,9 @@
 
 const DashboardNav = () => {
     return (
-        <nav className="bg-gray-800 p-4">
+        <nav className="bg-gray-800 p-4 flex justify-center gap-3">
             <h2 className="text-lg font-semibold text-center">Dashboard</h2>
+            <button>Home</button>
         </nav>
     );
 };
