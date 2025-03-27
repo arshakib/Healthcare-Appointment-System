9b77f034d582fb91172af54767d6e155c73a8c6f
jahidhasan6676 <jh18186676@gmail.com>
2025-03-27 15:54:07 +0600

    All device responsive dashboard design implement

---
+++ componenet/DashboardNav.jsx
@@ -0,0 +1,11 @@
+
+
+const DashboardNav = () => {
+    return (
+        <nav className="bg-gray-800 p-4">
+            <h2 className="text-lg font-semibold text-center">Dashboard</h2>
+        </nav>
+    );
+};
+
+export default DashboardNav;
