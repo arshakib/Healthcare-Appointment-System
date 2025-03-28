2e8c5b3d59cec55216b053fa1fed8066c51fa5e0
shakib <atking2240@gmail.com>
2025-03-29 00:15:45 +0600

    postcss.config.mjs updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,17 @@
-// const config = {
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
+
+// // eslint-disable-next-line import/no-anonymous-default-export
+// export default {
 //   plugins: {
-//     "@tailwindcss/postcss": {},
+//     tailwindcss: {},
+//     autoprefixer: {},
 //   },
 // };
-// export default config;
-
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
-};
+// // export default {
+// //   plugins: [require("tailwindcss"), require("autoprefixer")],
+// // };
