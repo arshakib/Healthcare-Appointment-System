73303196bbbb1b69f96a0cc739c189f189cc5fb8
shakib <atking2240@gmail.com>
2025-04-06 13:33:03 +0600

    All done

--- app/(private-route)/(without-nav-footer)/dashboard/page.jsx
+++ app/(private-route)/(without-nav-footer)/dashboard/page.jsx
@@ -4,41 +4,34 @@ import DoctorDashboard from "@/componenet/DoctorDashboard";
 import useUserRole from "@/componenet/hooks/useUserRole";
 import PatientDashboard from "@/componenet/PatientDashboard";
 
-
 export default function Dashboard() {
   const { role, loading } = useUserRole();
-  if (loading) return <h2 className="text-gray-800">Loading...</h2>
+  if (loading) return <h2 className="text-gray-800">Loading...</h2>;
   return (
     <>
-      
       <>
-        {
-          role === "doctor" &&
+        {role === "doctor" && (
           <div className="px-4 py-10">
-            <DoctorDashboard/>
+            <DoctorDashboard />
           </div>
-        }
+        )}
       </>
 
       <>
-        {
-          role === "patient" &&
-            <div className="px-4 py-10">
-              <PatientDashboard />
-            </div>
-        }
+        {role === "patient" && (
+          <div className="px-4 py-10">
+            <PatientDashboard />
+          </div>
+        )}
       </>
 
       <>
-        {
-          role === "admin" &&
+        {role === "admin" && (
           <div className="px-4 py-10">
             <AdminDashboard />
           </div>
-        }
+        )}
       </>
-
-
     </>
   );
 }
